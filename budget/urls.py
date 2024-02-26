6cc05c2d7250b56d0e481aadf46402e245481c6c
RobertOltean314 <olteanrobertadrian314@gmail.com>
2024-02-26 16:01:17 +0200

    Small schanges to the UI

--- budget/urls.py
+++ budget/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from budget import views
 
 # TODO O interfata mai frumoasa la budget + un model
+
+app_name = 'budget'
+
 urlpatterns = [
     path('budget/', views.budget_view, name='budget')
 ]
