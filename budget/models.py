4d6d74ad7b308260ccced334c2a4ed38f85fc64e
RobertOltean314 <olteanrobertadrian314@gmail.com>
2024-02-27 16:27:50 +0200

    More changes to the UI

--- budget/models.py
+++ budget/models.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+
+from transactions.models import Transaction
 
 
 # Create your models here.
@@ -13,3 +17,4 @@ class Budget(models.Model):
     def update_balance(self):
         self.current_balance = self.total_income - self.total_expenses
         self.save()
+        # TODO There is a bug, when a transaction is deleted, the budget doesn't update
