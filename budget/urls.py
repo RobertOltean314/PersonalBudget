a56d5e13b36853aa985576970357e9197ced2032
RobertOltean314 <olteanrobertadrian314@gmail.com>
2024-02-29 13:11:06 +0200

    UI Changes

--- budget/urls.py
+++ budget/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from budget import views
 
-# TODO O interfata mai frumoasa la budget + un model
+# TODO O interfata mai frumoasa la budget
 
 app_name = 'budget'
 
--- transactions/tests.py
+++ transactions/tests.py
@@ -2,4 +2,3 @@ from django.test import TestCase
 
 # Create your tests here.
 
-# TODO Sa facem teste pentru transactii
