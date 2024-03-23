9049fefa13c0879cda78248916cc153e0b8b1371
Robert Oltean <olteanrobertadrian314@gmail.com>
2024-03-23 13:44:04 +0200

    Solved a bug regarding budget creation when a new User is created

--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+
+from budget.models import Budget
 from .models import Profile
 
 
@@ -15,3 +17,9 @@ def create_profile(sender, instance, created, **kwargs):
 def save_profile(sender, instance, **kwargs):
     # Save the associated Profile instance whenever a User instance is saved
     instance.profile.save()  # This line is causing an error. See what needs to be done.
+
+@receiver(post_save, sender=User)
+def create_budget(sender, instance, created, **kwargs):
+    # When a new User instance is created, create a corresponding Budget instance
+    if created:
+        Budget.objects.create(user=instance)
